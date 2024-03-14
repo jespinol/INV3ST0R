8b696d40ba566dfd017ce84593c17927aeb482e7
jespinol <76534045+jespinol@users.noreply.github.com>
2024-03-14 15:27:53 -0400

    updated to temporarily remove spring security

--- src/main/java/com/jmel/inv3st0r/Inv3st0rApplication.java
+++ src/main/java/com/jmel/inv3st0r/Inv3st0rApplication.java
@@ -2,10 +2,13 @@ package com.jmel.inv3st0r;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Date;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Inv3st0rApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Inv3st0rApplication.class, args);
