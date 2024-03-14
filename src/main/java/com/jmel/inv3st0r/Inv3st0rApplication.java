ded31959a21e0677cb0cb2e4763663cdb81105d3
jespinol <76534045+jespinol@users.noreply.github.com>
2024-03-14 10:38:16 -0400

    modified front-end template SB Admin 2 from Start Bootstrap
    modified home page and account overview page

---
+++ src/main/java/com/jmel/inv3st0r/Inv3st0rApplication.java
@@ -0,0 +1,14 @@
+package com.jmel.inv3st0r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class Inv3st0rApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Inv3st0rApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmel/inv3st0r/Inv3st0rApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmel.inv3st0r;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inv3st0rApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
