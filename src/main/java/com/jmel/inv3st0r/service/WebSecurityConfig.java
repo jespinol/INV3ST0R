c89ef570bdd91ddf20460ffce7fa013af8bb46ee
jespinol <76534045+jespinol@users.noreply.github.com>
2024-03-19 11:13:20 -0400

    disabled requestCache to prevent redirect to random asset after authentication

--- src/main/java/com/jmel/inv3st0r/service/WebSecurityConfig.java
+++ src/main/java/com/jmel/inv3st0r/service/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -45,7 +46,7 @@ public class WebSecurityConfig {
                                 .permitAll()
                 )
                 .logout(logout -> logout.logoutSuccessUrl("/").permitAll()
-                );
+                ).requestCache(RequestCacheConfigurer::disable);
 
         return http.build();
     }
