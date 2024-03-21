bb53662631c3e13433a3a74352f188fe6dc72fe0
jespinol <76534045+jespinol@users.noreply.github.com>
2024-03-21 11:27:51 -0400

    created a stock owned model to keep track of quantity owned

---
+++ src/main/java/com/jmel/inv3st0r/model/Stock.java
@@ -0,0 +1,75 @@
+package com.jmel.inv3st0r.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "stocks")
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_id", nullable = false)
+    private Long accountId;
+
+    @Column(name = "symbol", nullable = false)
+    private String symbol;
+
+    @Column(name = "company", nullable = false)
+    private String company;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+
+    @Column(name = "last_price")
+    private double lastPrice;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getLastPrice() {
+        return lastPrice;
+    }
+
+    public void setLastPrice(double lastPrice) {
+        this.lastPrice = lastPrice;
+    }
+}
