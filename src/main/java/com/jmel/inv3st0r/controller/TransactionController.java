f3101657ed3894d7ced9878424506255537243ff
jespinol <76534045+jespinol@users.noreply.github.com>
2024-03-21 11:28:51 -0400

    updated controller to get and post sale and funding transactions, to update account balances, and minor refactoring

--- src/main/java/com/jmel/inv3st0r/controller/TransactionController.java
+++ src/main/java/com/jmel/inv3st0r/controller/TransactionController.java
@@ -1,8 +1,10 @@
 package com.jmel.inv3st0r.controller;
 
 import com.jmel.inv3st0r.model.Account;
+import com.jmel.inv3st0r.model.Stock;
 import com.jmel.inv3st0r.model.Transaction;
 import com.jmel.inv3st0r.repository.AccountRepository;
+import com.jmel.inv3st0r.repository.StockRepository;
 import com.jmel.inv3st0r.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -23,10 +24,31 @@ public class TransactionController {
     @Autowired
     private AccountRepository accountRepo;
 
-    @GetMapping(value = {"/purchase-stock"})
-    public String showPurchaseForm(Model model, @RequestParam("account-id") Long accountId) {
+    @Autowired
+    private StockRepository stockRepo;
+
+    private Account getAccount(Long accountId) {
         Optional<Account> account_opt = accountRepo.findById(accountId);
-        Account account = account_opt.get();
+        return account_opt.orElse(null);
+    }
+
+    @GetMapping(value = {"/fund"})
+    public String showFundForm(Model model, @RequestParam("account-id") Long accountId) {
+        Account account = getAccount(accountId);
+        model.addAttribute("accountInfo", account);
+
+        return "/fund";
+    }
+
+    @PostMapping(value = {"/fund"})
+    public String fundAccount(@RequestParam("account-id") Long accountId, @RequestParam("fund-amount") double fundAmount) {
+        updateAccountFund(accountId, fundAmount);
+        return "redirect:/overview?account-id=" + accountId;
+    }
+
+    @GetMapping(value = {"/purchase"})
+    public String showPurchaseForm(Model model, @RequestParam("account-id") Long accountId) {
+        Account account = getAccount(accountId);
         model.addAttribute("accountInfo", account);
 
         model.addAttribute("newTransaction", new Transaction());
@@ -34,43 +56,122 @@ public class TransactionController {
         return "/purchase";
     }
 
-    @PostMapping(value = {"/purchase-stock"})
+    @PostMapping(value = {"/purchase"})
     public String buyStock(Transaction transaction) {
-        transaction.setLastCurrentPrice(transaction.getPurchasePrice());
-
         transactionRepo.save(transaction);
-        updateAccountPurchase(transaction);
+        updateAccountTransaction(transaction);
+        updateStockRecord(transaction);
 
         return "redirect:/overview?account-id=" + transaction.getAccountId();
     }
 
-    public static ArrayList<Transaction> listAccountTransactions(TransactionRepository repo, Account account, boolean showAll) {
-        ArrayList<Transaction> transactionList = new ArrayList<>();
-        repo.findAll().forEach(transaction -> {
-            if (transaction.getAccountId().equals(account.getId())) {
-                transactionList.add(transaction);
-            }
-        });
+    @GetMapping(value = {"/sell"})
+    public String showSaleForm(Model model, @RequestParam("account-id") Long accountId) {
+        Account account = getAccount(accountId);
+        model.addAttribute("accountInfo", account);
 
-        if (!showAll) {
-            List<Transaction> truncated = transactionList.subList(Math.max(transactionList.size() - 5 , 0), transactionList.size());
+        model.addAttribute("newTransaction", new Transaction());
 
-            return new ArrayList<>(truncated);
-        }
+        model.addAttribute("ownedStocks", getOwnedStocks(accountId));
 
-        return transactionList;
+        return "/sell";
     }
 
-    private void updateAccountPurchase(Transaction transaction) {
-        Optional<Account> account_opt = accountRepo.findById(transaction.getAccountId());
-        Account account = account_opt.get();
+    private ArrayList<Stock> getOwnedStocks(Long accountId) {
+        return stockRepo.findAllByAccountId(accountId);
+    }
+
+    @PostMapping(value = {"/sell"})
+    public String sellStock(Transaction transaction) {
+        transaction.setTransactionType(Transaction.TransactionType.SELL);
+        transactionRepo.save(transaction);
+        updateAccountTransaction(transaction);
+        updateStockRecord(transaction);
+
+        return "redirect:/overview?account-id=" + transaction.getAccountId();
+    }
+
+    private void updateAccountFund(Long accountId, double fundAmount) {
+        Account account = getAccount(accountId);
+        if (account == null) {
+            System.out.println("updateAccountFund: Account not found");
+            return;
+        }
+
+        account.setCashBalance(account.getCashBalance() + fundAmount);
+        accountRepo.save(account);
+    }
+
+    private void updateAccountTransaction(Transaction transaction) {
+        Account account = getAccount(transaction.getAccountId());
+        if (account == null) {
+            System.out.println("updateAccountTransaction: Account not found");
+            return;
+        }
 
         double oldCash = account.getCashBalance();
         double oldInvested = account.getInvestedBalance();
         double transactionCost = transaction.getPurchasePrice() * transaction.getQuantity();
-        account.setCashBalance(oldCash - transactionCost);
-        account.setInvestedBalance(oldInvested + transactionCost);
+        if (transaction.getTransactionType() == Transaction.TransactionType.BUY) {
+            account.setCashBalance(oldCash - transactionCost);
+            account.setInvestedBalance(oldInvested + transactionCost);
+        } else {
+            account.setCashBalance(oldCash + transactionCost);
+            account.setInvestedBalance(oldInvested - transactionCost);
+        }
 
         accountRepo.save(account);
+    }
+
+    private void updateStockRecord(Transaction transaction) {
+        Optional<Stock> stock_opt = stockRepo.findBySymbolAndAccountId(transaction.getSymbol(), transaction.getAccountId());
+        if (stock_opt.isPresent()) {
+            Stock stock = stock_opt.get();
+            if (transaction.getTransactionType() == Transaction.TransactionType.BUY) {
+                stock.setQuantity(stock.getQuantity() + transaction.getQuantity());
+            } else {
+                stock.setQuantity(stock.getQuantity() - transaction.getQuantity());
+            }
+            stockRepo.save(stock);
+            return;
+        }
+
+        stockRepo.save(createNewStockRecord(transaction));
+    }
+
+    private Stock createNewStockRecord(Transaction transaction) {
+        Stock stock = new Stock();
+        stock.setAccountId(transaction.getAccountId());
+        stock.setSymbol(transaction.getSymbol());
+        stock.setCompany(transaction.getCompany());
+        stock.setQuantity(transaction.getQuantity());
+        stock.setLastPrice(transaction.getPurchasePrice());
+
+        return stock;
+    }
+
+    public static ArrayList<Transaction> listAccountTransactions(TransactionRepository repo, Account account, boolean showAll) {
+        if (showAll) {
+            return repo.findAllByAccountId(account.getId());
+        }
+
+        return repo.findTop5ByAccountIdOrderByTransactionDateDesc(account.getId());
+        // TODO: truncated list is populated but does not show in /home
+
+//        ArrayList<Transaction> transactionList = new ArrayList<>();
+//
+//        repo.findAll().forEach(transaction -> {
+//            if (transaction.getAccountId().equals(account.getId())) {
+//                transactionList.add(transaction);
+//            }
+//        });
+//
+//        if (!showAll) {
+//            List<Transaction> truncated = transactionList.subList(Math.max(transactionList.size() - 5, 0), transactionList.size());
+//
+//            return new ArrayList<>(truncated);
+//        }
+//
+//        return transactionList;
     }
 }
