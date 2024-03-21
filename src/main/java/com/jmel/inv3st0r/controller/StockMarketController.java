b11e4120e725c1a526fea9a12d5c542807cdda45
jespinol <76534045+jespinol@users.noreply.github.com>
2024-03-21 11:24:20 -0400

    renamed class

--- src/main/java/com/jmel/inv3st0r/controller/StockMarketController.java
+++ src/main/java/com/jmel/inv3st0r/controller/StockMarketController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class StockController {
+public class StockMarketController {
 
     @Autowired
     private StockService stockService;
