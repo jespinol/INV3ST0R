85e52d8a0a2a26f3fb1578d179e6c15dc99508de
jespinol <76534045+jespinol@users.noreply.github.com>
2024-03-22 12:58:04 -0400

    added code to handle users without accounts

--- src/main/resources/static/js/chart-pie.js
+++ src/main/resources/static/js/chart-pie.js
@@ -5,32 +5,38 @@ Chart.defaults.global.defaultFontColor = '#858796';
 
 const ctxPieChart = document.getElementById("pieChart");
 
-const pieChart = new Chart(ctxPieChart, {
-    type: 'doughnut',
-    data: {
-        labels: pieChartLabels,
-        datasets: [{
-            data: pieChartData,
-            backgroundColor: pieChartBackgroundColors,
-            hoverBackgroundColor: pieChartHoverBackGroundColors,
-            hoverBorderColor: "rgba(234, 236, 244, 1)",
-        }],
-    },
-    options: {
-        maintainAspectRatio: false,
-        tooltips: {
-            backgroundColor: "rgb(255,255,255)",
-            bodyFontColor: "#858796",
-            borderColor: '#dddfeb',
-            borderWidth: 1,
-            xPadding: 15,
-            yPadding: 15,
-            displayColors: true,
-            caretPadding: 10,
+if (pieChartLabels.length === 0) {
+    const text = "No data available";
+    document.getElementById('pieChartLegend').innerText = text;
+    document.getElementById('pieChartCanvas').innerHTML = `<div class='text-center text-muted small'>${text}</div>`;
+} else {
+    const pieChart = new Chart(ctxPieChart, {
+        type: 'doughnut',
+        data: {
+            labels: pieChartLabels,
+            datasets: [{
+                data: pieChartData,
+                backgroundColor: pieChartBackgroundColors,
+                hoverBackgroundColor: pieChartHoverBackGroundColors,
+                hoverBorderColor: "rgba(234, 236, 244, 1)",
+            }],
         },
-        legend: {
-            display: false
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: true,
+                caretPadding: 10,
+            },
+            legend: {
+                display: false
+            },
+            cutoutPercentage: 70,
         },
-        cutoutPercentage: 70,
-    },
-});
+    });
+}
